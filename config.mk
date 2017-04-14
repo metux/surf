d8f2ed4b335c2a50870e4ecc02d995cdb3472979
Enrico Weigelt, metux IT consult <enrico.weigelt@gr13.net>
2017-04-14 15:39:58 +0200

    config.mk: make build tools overridable via env
    Build systems (especially for isolated or embedded targets)
    need a generic way to pass their tool commands (compiler,
    pkg-config, etc)

--- config.mk
+++ config.mk
@@ -3,6 +3,10 @@ VERSION = 2.0
 
 # Customize below to fit your system
 
+# build tools
+CC         ?= cc
+PKG_CONFIG ?= pkg-config
+
 # paths
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
@@ -11,8 +15,8 @@ LIBPREFIX = ${PREFIX}/lib/surf
 X11INC = /usr/X11R6/include
 X11LIB = /usr/X11R6/lib
 
-GTKINC = `pkg-config --cflags gtk+-3.0 webkit2gtk-4.0`
-GTKLIB = `pkg-config --libs gtk+-3.0 webkit2gtk-4.0`
+GTKINC = `$(PKG_CONFIG) --cflags gtk+-3.0 webkit2gtk-4.0`
+GTKLIB = `$(PKG_CONFIG) --libs gtk+-3.0 webkit2gtk-4.0`
 
 # includes and libs
 INCS = -I. -I/usr/include -I${X11INC} ${GTKINC}
@@ -26,6 +30,3 @@ LDFLAGS = -s ${LIBS}
 # Solaris
 #CFLAGS = -fast ${INCS} -DVERSION=\"${VERSION}\"
 #LDFLAGS = ${LIBS}
-
-# compiler and linker
-CC = cc
